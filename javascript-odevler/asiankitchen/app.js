9d4a7fe15945ef78e6df40f2c2d2eb9be2136993
Emirhan ÇİRMENCİK <19332652013@ogrenci.btu.edu.tr>
2021-10-20 02:40:16 +0300

    Ürünler eklendi, butonlar çalışır hale getirildi.

--- javascript-odevler/asiankitchen/app.js
+++ javascript-odevler/asiankitchen/app.js
@@ -73,8 +73,62 @@ const menu = [
   },
 ];
 
-// TODO: Kategori list yap.
+let categories = ["All"]; // Kategoriler'e tümünü seçeneğini ekler.
+categories.push(...[...new Set(menu.map((item) => item.category))]); // Category isimlerini toplar ve aynılarını siler.
+const BTNLIST = document.querySelector("div.btn-container"); // Butonlar elemente eklenecek.
+const ITEMLIST = document.querySelector("div.section-center"); // Ürünler bu elemente eklenecek.
 
-// TODO: Ürünleri Yerleştir.
+const KOREA = menu.filter((item) => item.category == "Korea"); // Kategorisi Kore olanları ayırır.
+const CHINA = menu.filter((item) => item.category == "China"); // Kategorisi Çin olanları ayırır.
+const JAPAN = menu.filter((item) => item.category == "Japan"); // Kategorisi Japonya olanları ayırır.
 
-// TODO: Kategoriyi çalışır hale getir.
+const MENUS = [menu, KOREA, JAPAN, CHINA]; // Tüm kategorilere daha rahat erişim sağlamak için Array altında toplandı.
+
+for (let i = 0; i < categories.length; i++) { // Kategori butonlarını oluşturur.
+  const BTN = document.createElement("BUTTON");
+  BTN.classList.add("btn", "btn-item", "btn-outline-dark"); 
+  BTN.innerHTML = categories[i];
+  BTN.onclick = () => { // Butonlara basıldığında ilgili kategoriye göre ürünleri listelemesi için fonksiyon çağırır.
+    createItem(MENUS[i]);
+  };
+  BTNLIST.appendChild(BTN); // Butonları ekler.
+}
+
+function createItem(itemList) {
+  ITEMLIST.innerHTML = "";
+  for (let i = 0; i < itemList.length; i++) {
+    const ITEM = document.createElement("div"); // İlgili elemanları oluşturur..
+    const IMG = document.createElement("img");
+    const MENUINFO = document.createElement("div");
+    const MENUTITLE = document.createElement("div");
+    const HEADER = document.createElement("h4");
+    const HEADER_TEXT = document.createTextNode(itemList[i].title);
+    const PRICE = document.createElement("h4");
+    const PRICE_TEXT = document.createTextNode(itemList[i].price);
+    const MENUTEXT = document.createElement("div");
+    const DESC = document.createTextNode(itemList[i].desc);
+
+    ITEM.classList.add("menu-items", "col-lg-6", "col-sm-12");
+    ITEM.appendChild(IMG);
+    ITEM.appendChild(MENUINFO);
+    IMG.classList.add("photo");
+    IMG.alt = itemList[i].title;
+    IMG.src = itemList[i].img;
+    MENUINFO.classList.add("menu-info");
+    MENUINFO.appendChild(MENUTITLE);
+    MENUINFO.appendChild(MENUTEXT);
+    MENUTITLE.classList.add("menu-title");
+    MENUTITLE.appendChild(HEADER);
+    MENUTITLE.appendChild(PRICE);
+    HEADER.appendChild(HEADER_TEXT);
+    PRICE.classList.add("price");
+    PRICE.appendChild(PRICE_TEXT);
+    MENUTEXT.classList.add("menu-text");
+    MENUTEXT.appendChild(DESC);
+
+    ITEMLIST.appendChild(ITEM);
+  }
+
+  const DIV = document.createElement("div");
+}
+createItem(menu); // Body load olduğunda bütün ürünleri göstermek için çağırılır.
