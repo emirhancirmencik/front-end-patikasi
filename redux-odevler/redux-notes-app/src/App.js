364231749052984cd462fe1a4bdd4f3b3a9b88e1
Emirhan ÇİRMENCİK <19332652013@ogrenci.btu.edu.tr>
2022-02-15 23:44:53 +0300

    Overflow-x fixed.

--- redux-odevler/redux-notes-app/src/App.js
+++ redux-odevler/redux-notes-app/src/App.js
@@ -6,11 +6,11 @@ import Content from "./components/Content";
 function App() {
   return (
     <div className="App">
-      <div className="row">
-        <div className="col-12 form">
+      <div className="row me-0 ms-0">
+        <div className="col-12 form ">
           <Form />
         </div>
-        <div className="col-12 filter h-100">
+        <div className="col-12 filter h-100 ps-0 pe-0">
           <Content />
         </div>
       </div>
